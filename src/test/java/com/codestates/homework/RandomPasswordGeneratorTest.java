4981c05989178bb8cf113df0e12a1c3447c79bd7
skawlgns1 <dydcjsgid@naver.com>
2023-03-03 15:43:52 +0900

    push

--- src/test/java/com/codestates/homework/RandomPasswordGeneratorTest.java
+++ src/test/java/com/codestates/homework/RandomPasswordGeneratorTest.java
@@ -1,12 +1,73 @@
 package com.codestates.homework;
 
+import com.codestates.helper.RandomPasswordGenerator;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RandomPasswordGeneratorTest {
-    @DisplayName("실습 3: 랜덤 패스워드 생성 테스트")
+
+    static int numberOfUpperCaseLetters = 1;   //생성될 패스워드 문자열에서 알파벳 대문자의 개수
+    static int numberOfLowerCaseLetters = 2;  //생성될 패스워드 문자열에서 알파벳 소문자의 개수
+    static int numberOfNumeric = 3;   //생성될 패스워드 문자열에서 0 이상인 숫자의 개수
+    static int numberOfSpecialChars = 4;   //생성될 패스워드 문자열에서 특수문자의 개수
+    static String result = RandomPasswordGenerator.generate(numberOfUpperCaseLetters, numberOfLowerCaseLetters,
+            numberOfNumeric,numberOfSpecialChars);
+
+    @DisplayName("실습 3-1: 생성된 패스워드의 길이가 입력한 파라미터 숫자의 합과 일치하는지 검증(Assertion)")
     @Test
-    public void generateTest() {
-        // TODO 여기에 테스트 케이스를 작성해주세요.
+    public void generateTest1() {
+
+        int actual = result.length();
+        int expected = numberOfUpperCaseLetters+numberOfLowerCaseLetters+numberOfNumeric+numberOfSpecialChars;
+        assertEquals(actual,expected);
+
+    }
+    @DisplayName("실습 3-2: 생성된 패스워드의 ‘알파벳 대문자’ 개수가 입력한 파라미터(numberOfUpperCaseLetters ) 숫자와 일치하는지 검증하세요")
+    @Test
+    public void generateTest2() {
+    char[] Array = result.toCharArray();
+    int actual = 0;
+    for(char c : Array){
+        if(Character.isUpperCase(c)) actual++;
+    }
+    int expected = numberOfUpperCaseLetters;
+        assertEquals(actual,expected);
+    }
+    @DisplayName("실습 3-3: 생성된 패스워드의 ‘알파벳 소문자’ 개수가 입력한 파라미터(numberOfLowerCaseLetters) 숫자와 일치하는지 검증하세요.")
+    @Test
+    public void generateTest3() {
+        char[] Array = result.toCharArray();
+        int actual = 0;
+        for(char c : Array){
+            if(Character.isLowerCase(c)) actual++;
+        }
+        int expected = numberOfLowerCaseLetters;
+        assertEquals(actual,expected);
+    }
+    @DisplayName("실습 3-4: 생성된 패스워드의 ‘0 이상인 숫자’의 개수가 입력한 파라미터(numberOfNumeric) 숫자와 일치하는지 검증하세요.")
+    @Test
+    public void generateTest4() {
+        char[] Array = result.toCharArray();
+        int actual = 0;
+        for(char c : Array){
+            if(Character.isDigit(c)) actual++;
+        }
+        int expected = numberOfNumeric;
+        assertEquals(actual,expected);
+    }
+    @DisplayName("실습 3-5: 생성된 임시 패스워드의 ‘특수문자’ 개수가 입력한 파라미터(numberOfSpecialChars) 숫자와 일치하는지 검증하세요.")
+    @Test
+    public void generateTest5() {
+        char[] Array = result.toCharArray();
+        int actual = 0;
+        for(char c : Array){
+            int result = (int)c;
+            if(result>32 && result<48) actual++;
+        }
+        int expected = numberOfSpecialChars;
+        assertEquals(actual,expected);
     }
 }
--- src/test/java/com/codestates/homework/StampCalculatorTest.java
+++ src/test/java/com/codestates/homework/StampCalculatorTest.java
@@ -1,18 +1,40 @@
 package com.codestates.homework;
 
+import com.codestates.helper.StampCalculator;
+import com.codestates.order.entity.Order;
+import com.codestates.order.entity.OrderCoffee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StampCalculatorTest {
     @Test
     @DisplayName("실습1: 스탬프 카운트 계산 단위 테스트")
     public void calculateStampCountTest() {
-        // TODO 여기에 테스트 케이스를 작성해주세요.
+        int nowCount = 3;
+        int earned = 5;
+        int actual = StampCalculator.calculateStampCount(nowCount, earned);
+        int expected = 8;
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("실습1: 주문 후 누적 스탬프 카운트 계산 단위 테스트")
     public void calculateEarnedStampCountTest(){
-        // TODO 여기에 테스트 케이스를 작성해주세요.
+        Order order = new Order();
+        OrderCoffee orderCoffee1 = new OrderCoffee();
+        OrderCoffee orderCoffee2 = new OrderCoffee();
+        orderCoffee1.setQuantity(3);
+        orderCoffee2.setQuantity(5);
+        order.getOrderCoffees().add(orderCoffee1);
+        order.getOrderCoffees().add(orderCoffee2);
+
+        int actual = StampCalculator.calculateEarnedStampCount(order);
+        int expected = 8;
+        assertEquals(expected, actual);
     }
+
+
+
 }
